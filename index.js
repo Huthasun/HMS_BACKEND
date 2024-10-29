d820426d926f845909144827438f944248b08744
Huthasan Odugu <nanihuthasun@gmail.com>
2024-10-29 12:20:23 +0530

    url change

--- index.js
+++ index.js
@@ -200,7 +200,7 @@ mongoose
   .connect("mongodb+srv://saikiranautomac:jXGLmMQ7RKuO00uP@cluster0.qssqx4c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("Connected to database!");
-    app.listen(80,'0.0.0.0', () => {
+    app.listen(3000,'localhost', () => {
       console.log("Server is running on port 80");
     });
   })

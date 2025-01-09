798b2ef1b6f5e1098d7b694684f54c4f132ad2fa
Huthasan Odugu <nanihuthasun@gmail.com>
2025-01-09 14:05:33 +0530

    input fileds change

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ mongoose
   .then(() => {
     console.log("Connected to database!");
     app.listen(3000,'localhost', () => {
-      console.log("Server is running on port 3000");
+      console.log("Server is running on port 80");
     });
   })
   .catch((error) => {
